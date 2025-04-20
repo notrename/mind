494bf15062bec87cd2b73af37170a24eac223040
notrename <ogmnogoel@gmail.com>
2025-04-20 16:04:56 +0300

    fix lint

--- tests/google/test_google.py
+++ tests/google/test_google.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_request_get__positive():
-    response = requests.get('https://www.google.ru/')
+    response = requests.get("https://www.google.ru/")
     assert response.status_code == 200
--- tests/yandex/test_yandex.py
+++ tests/yandex/test_yandex.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_request_get__positive():
-    response = requests.get('https://ya.ru')
+    response = requests.get("https://ya.ru")
     assert response.status_code == 200
